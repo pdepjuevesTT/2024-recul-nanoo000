c8a4ff94fa0784ce9d73689f2f4f70646e3c55ac
Ignacio Martín Córdoba <131314817+nanoo000@users.noreply.github.com>
2024-12-07 10:10:21 -0300

    Update solucion.pl

--- solucion.pl
+++ solucion.pl
@@ -1,1 +1,54 @@
-% Solución en Prolog
+espacioDePropiedad(juan,120).
+espacioDePropiedad(fer,110).
+espacioDePropiedad(rocio,120).
+
+ambientes(nico,3,banios(2)).
+ambientes(nico,3,banios(1)).
+ambientes(vale,4,banios(1)).
+
+viveEn(alf,almagro).
+viveEn(nico,almagro).
+viveEn(juan,almagro).
+viveEn(juan,almagro).
+viveEn(vale,flores).
+viveEn(fer,flores).
+
+loft(julian,2000).
+
+%Punto 2
+esBarrioCopado(Barrio,Persona):-
+    viveEn(Persona,Barrio),
+    forall((viveEn(Persona,Barrio)), tienePropiedadCopada(Persona)).
+    
+tienePropiedadCopada(Persona):-
+    espacioDePropiedad(Persona,MetrosCuadrados),
+    MetrosCuadrados>100.
+tienePropiedadCopada(Persona):-
+    ambientes(Persona,Ambientes,_),
+    Ambientes>3.
+tienePropiedadCopada(Persona):-
+    ambientes(Persona,_,banios(CantidadDeBanios)),
+    CantidadDeBanios<1.
+tienePropiedadCopada(Persona):-
+    loft(Persona,AnioDeConstruccion),
+    AnioDeConstruccion>2015.
+
+
+%Punto 3
+esBarrioCaro(Barrio):-
+ viveEn(Persona,Barrio),
+ forall(viveEn(Persona,Barrio),tienePropiedadBarata(Persona)).
+
+
+tienePropiedadBarata(Persona):-
+    loft(Persona,AnioDeConstruccion),
+    AnioDeConstruccion<2005.
+tienePropiedadBarata(Persona):-
+    espacioDePropiedad(Persona,MetrosCuadrados),
+    MetrosCuadrados<90.
+tienePropiedadBarata(Persona):-
+    ambientes(Persona,Ambientes,_),
+    Ambientes=<2.
+
+%Punto 4
+
